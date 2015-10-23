986e99c2237416c06a8c7b1e8496207aa2edb834
cbejensen <cbejensen@gmail.com>
2015-10-22 19:40:11 -0600

    fixed small type-o

--- js/parseService.js
+++ js/parseService.js
@@ -12,7 +12,7 @@ app.service('parseService', function($http){
   //On the line below create the postData method. This method will add data to the parse backend.
   //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
   //Because we're making a POST request, we need a way to tell parse the data we want to give it, 
-    //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
+    //in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
   
   //postData method here
